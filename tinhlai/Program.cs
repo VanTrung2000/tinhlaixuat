d561d98c0c57c22da2df000bcb6f505106d010f7
VanTrung2000 <70925652+VanTrung2000@users.noreply.github.com>
2020-09-09 19:12:36 +0700

    Update Program.cs

--- tinhlai/Program.cs
+++ tinhlai/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace tinhlai
 {
@@ -9,19 +9,22 @@ namespace tinhlai
             double money = 1.0;
             int month = 1;
             double intersetRate = 1.0;
-            Console.WriteLine("Nhan so tien gui: ");
+            Console.WriteLine("Nhap so tien gui: ");
             money = Double.Parse(Console.ReadLine());
-            Console.WriteLine("Nhan so thang gui: ");
+            Console.WriteLine("Nhap so thang gui: ");
             month = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Nhan lai xuat: ");
+            Console.WriteLine("Nhap lai xuat: ");
             intersetRate = Double.Parse(Console.ReadLine());
             double totalInterset = 0;
-            for (int i = 0; i < month; i++)
-            {
-                totalInterset = money * (intersetRate / 100) / 12 * 3;
-            }
-            Console.WriteLine("Total of interset: " + totalInterset);
+            laixuat(money, month, intersetRate);
             Console.ReadKey();
+        }
+        static double laixuat(double a, int b, double c){
+            double d = a * (c / 100) / 12 * 3;
+            Console.WriteLine("Total of interset: " + d);
+            return d;
+
         }
     }
 }
+
